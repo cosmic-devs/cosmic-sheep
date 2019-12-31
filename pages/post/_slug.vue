7651305eed197e51f8d2eec3153bd6e241a208a5
Pablo Miceli <d1urno@gmx.com>
2019-12-31 20:28:51 -0300

    Replace posts id route by slug

---
+++ pages/post/_slug.vue
@@ -0,0 +1,32 @@
+<template>
+  <div v-if="post" class="container markdown">
+    <div class="mb-10" v-html="$md.render(post.body)"></div>
+    <p>
+      <NuxtLink to="/">
+        Home page
+      </NuxtLink>
+    </p>
+  </div>
+</template>
+
+<script>
+  import post from '~/apollo/queries/post'
+
+  export default {
+    apollo: {
+      post: {
+        query: post,
+        prefetch: ({route}) => ({slug: route.params.slug}),
+        variables() {
+          return {slug: this.$route.params.slug}
+        }
+      }
+    },
+    methods: {},
+    head() {
+      return {
+        title: (this.post ? `Cosmic Sheep | ${this.post.title}` : 'Loading...')
+      }
+    }
+  }
+</script>
