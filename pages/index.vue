fd3032dc1a199c74d6ffdd6d75b01877ff975641
Pablo Miceli <d1urno@gmx.com>
2020-01-03 09:46:05 -0300

    Add background color to show on page load

--- pages/index.vue
+++ pages/index.vue
@@ -96,6 +96,13 @@
     padding-bottom: 0.125rem;
   }
 
+  header {
+    background: rgb(2,0,36);
+    background: linear-gradient(180deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 60%, rgba(0,148,179,1) 100%);
+    height: 100%;
+    width: 100%;
+  }
+
   .markdown {
     @apply text-gray-900 leading-normal break-words;
   }
