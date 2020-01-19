acfbcc86acadb1538afe164e85a7d54a93633184
anakais <v8.alexandre@gmail.com>
2020-01-19 15:26:54 -0300

    Fix height and width of background

--- pages/index.vue
+++ pages/index.vue
@@ -95,7 +95,5 @@
   #index header {
     background: rgb(2, 0, 36);
     background: linear-gradient(180deg, rgba(2, 0, 36, 1) 0%, rgba(9, 9, 121, 1) 60%, rgba(0, 148, 179, 1) 100%);
-    height: 100%;
-    width: 100%;
   }
 </style>
