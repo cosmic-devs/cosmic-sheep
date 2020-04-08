f1185c3137ad9cc31381984b3b174722f5b4952f
Pablo Miceli <d1urno@gmx.com>
2020-04-08 19:34:00 -0300

    Append commented Tailwind variants for reference

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,6 +22,7 @@ module.exports = {
       }
     }
   },
+  // variants: ['responsive', 'group-hover', 'focus-within', 'first', 'last', 'odd', 'even', 'hover', 'focus', 'active', 'visited', 'disabled'],
   variants: {},
   plugins: []
 }
